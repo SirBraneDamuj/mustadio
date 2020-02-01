9caa3a1a7a78d6ae723641e2648f3a1d1b033df2
Zach Thacker <zpthacker@gmail.com>
2020-02-01 02:38:00 -0500

    remove line endings eslint rule because WINDOWS -> LINUX = bad

--- .eslintrc.js
+++ .eslintrc.js
@@ -110,10 +110,7 @@ module.exports = {
             }
         ],
         "line-comment-position": "error",
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
+        "linebreak-style": "off",
         "lines-around-comment": "error",
         "lines-around-directive": "error",
         "lines-between-class-members": "error",
