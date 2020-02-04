b2a478eaad405d090fbc99aa1fd1d6b3fb0a0df7
Zach Thacker <zpthacker@gmail.com>
2020-02-04 00:32:13 -0500

    use a different data strategy for db vs fftbg dumps

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,4 @@
-if (process.env.FFTBG_DATA_STRATEGY === 'real') {
+if (process.env.DB_DATA_STRATEGY === 'real') {
     module.exports = require('./real');
 } else {
     module.exports = require('./fake');
