6606f73a57c2a9466665f398dc9efd1d974e218c
Zach Thacker <zpthacker@gmail.com>
2020-04-03 23:52:37 -0400

    padding

--- client/src/components/match/Match.js
+++ client/src/components/match/Match.js
@@ -11,13 +11,13 @@ export default function Match() {
         return null;
     }
     return (
-        <>
+        <div className='p-3'>
             <div className='d-flex flex-column'>
                 <h2>{context.tournament.tournamentId}</h2>
                 <h2>{`${team1.name} vs ${team2.name}`}</h2>
                 {context.currentMap && <MapPreview mapNumber={context.currentMap} />}
             </div>
-            <div className='d-flex match-container'>
+            <div className='d-flex match-container mt-3'>
                 <div className='align-self-start mr-1'>
                     <h2>{team1.name} team</h2>
                     <Team team={team1} side='left' otherTeam={team2} />
@@ -27,6 +27,6 @@ export default function Match() {
                     <Team team={team2} side='right' otherTeam={team1} />
                 </div>
             </div>
-        </>
+        </div>
     )
 }
