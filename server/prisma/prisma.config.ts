11abb79059418d53aac66fa2d07832c2815ad0d4
Zach Thacker <zpthacker@gmail.com>
2026-02-27 19:24:51 -0500

    move prisma config

--- server/prisma/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { defineConfig } from 'prisma/config';
-
-export default defineConfig({
-  schema: './schema.prisma',
-  datasource: {
-    url: process.env.DATABASE_URL ?? '',
-  },
-});
