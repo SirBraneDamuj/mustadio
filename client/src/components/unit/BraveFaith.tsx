f558c47e9e54c77cd97ded21433df42341ee1469
Zach Thacker <zpthacker@gmail.com>
2026-04-12 09:10:35 -0400

    fix portrait sinking when unit-basic column is shorter than row

--- client/src/components/unit/BraveFaith.tsx
+++ client/src/components/unit/BraveFaith.tsx
@@ -7,7 +7,7 @@ export default function BraveFaith({ brave, faith }: BraveFaithProps) {
     const isNotable = (val: number) => val < 50 || val > 70;
 
     return (
-        <div className='grow'>
+        <div>
             <span className={isNotable(brave) ? 'font-bold' : ''}>B: {brave}</span>
             <span> / </span>
             <span className={isNotable(faith) ? 'font-bold' : ''}>F: {faith}</span>
